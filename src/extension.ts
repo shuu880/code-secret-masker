aa9b29b02986f06c05037e12bae35f3884c481f1
shuu880 <shuu880.12@gmail.com>
2024-04-13 06:04:36 +0900

    Supported arrays in JSON

--- src/extension.ts
+++ src/extension.ts
@@ -14,12 +14,14 @@ export function activate(context: vscode.ExtensionContext) {
                     return;
                 }
                 const config = JSON.parse(data);
-                const key = config.key;
+                const keys = config.keys;
                 const text = document.getText();
                 const updatedText = text.split('\n').map(line => {
-                    const index = line.indexOf(key);
-                    if (index !== -1) {
-                        return line.substring(0, index + key.length) + '*'.repeat(line.length - index - key.length);
+                    for (const key of keys) {
+                        const index = line.indexOf(key);
+                        if (index !== -1) {
+                            return line.substring(0, index + key.length) + '*'.repeat(line.length - index - key.length);
+                        }
                     }
                     return line;
                 }).join('\n');
@@ -47,12 +49,14 @@ export function activate(context: vscode.ExtensionContext) {
                     return;
                 }
                 const config = JSON.parse(data);
-                const key = config.key;
+                const keys = config.keys;
                 const text = document.getText();
                 const updatedText = text.split('\n').map(line => {
-                    const index = line.indexOf(key);
-                    if (index !== -1) {
-                        return line.substring(0, index + key.length) + '*'.repeat(line.length - index - key.length);
+                    for (const key of keys) {
+                        const index = line.indexOf(key);
+                        if (index !== -1) {
+                            return line.substring(0, index + key.length) + '*'.repeat(line.length - index - key.length);
+                        }
                     }
                     return line;
                 }).join('\n');
@@ -73,42 +77,3 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
-
-
-
-
-// import * as vscode from 'vscode';
-
-// export function activate(context: vscode.ExtensionContext) {
-//     let disposable = vscode.commands.registerCommand('extension.maskSensitiveInfo', () => {
-//         const editor = vscode.window.activeTextEditor;
-//         if (editor) {
-//             const document = editor.document;
-//             const text = document.getText();
-//             let sensitiveWords = ["key", "pass"]; // 初期の指定文字列
-
-//             // JSONファイルから指定文字列を読み込む
-//             // ここでは、"sensitiveWords.json"というファイル名を仮定しています
-//             const fs = require('fs');
-//             if (fs.existsSync('sensitiveWords.json')) {
-//                 let rawdata = fs.readFileSync('sensitiveWords.json');
-//                 sensitiveWords = JSON.parse(rawdata);
-//             }
-
-//             sensitiveWords.forEach(word => {
-//                 const regex = new RegExp(`(${word})(.*)`, 'g');
-//                 const maskedText = text.replace(regex, (match, p1, p2) => {
-//                     return p1 + p2.replace(/\S/g, '*');
-//                 });
-
-//                 fs.writeFileSync('maskedText.txt', maskedText);
-//             });
-
-//             vscode.window.showInformationMessage('Sensitive information has been masked!');
-//         }
-//     });
-
-//     context.subscriptions.push(disposable);
-// }
-
-// export function deactivate() {}
